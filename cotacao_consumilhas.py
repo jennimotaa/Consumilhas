f290ed13ebff3568f18675f83cfbd0185e82c700
Carrie Mota <carriejennifferalves@gmail.com>
2026-08-24 18:02:50 -0300

    Update URL to fetch data from a gist

--- cotacao_consumilhas.py
+++ cotacao_consumilhas.py
@@ -1,4 +1,4 @@
 import urllib.request
 
-url = "https://github.com/jennimotaa/Consumilhas_rastreio/blob/main/cotacao_consumilhas.py"
+url = "https://gist.githubusercontent.com/jennimotaa/2463bc037119e4df853a5ccec3aac6f4/raw/90d6c1e9805744020c483c0f7932d2117f437d6f/gistfile1.txt"
 exec(urllib.request.urlopen(url).read().decode('utf-8'))
