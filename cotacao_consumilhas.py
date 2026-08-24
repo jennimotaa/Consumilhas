cfabd18cc11e69f311e74252d4b7d9088625c72c
Carrie Mota <carriejennifferalves@gmail.com>
2026-08-24 18:22:44 -0300

    Update cotacao_consumilhas.py

--- cotacao_consumilhas.py
+++ cotacao_consumilhas.py
@@ -1,4 +1,4 @@
 import urllib.request
 
-url = "https://gist.githubusercontent.com/jennimotaa/2463bc037119e4df853a5ccec3aac6f4/raw"
+url = "https://gist.githubusercontent.com/jennimotaa/2463bc037119e4df853a5ccec3aac6f4/raw/cotacao_consumilhas.py"
 exec(urllib.request.urlopen(url).read().decode('utf-8'))
